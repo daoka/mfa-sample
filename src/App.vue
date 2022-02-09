52dd275b04c4a9d28c60ed8420ec0d622b60e9e6
daoka <daoka@opening-line.co.jp>
2022-02-09 12:59:05 +0900

    reCAPTCHAがうごくとこまで

--- src/App.vue
+++ src/App.vue
@@ -1,18 +1,95 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+  <div>
+    <p>Auth</p>
+    <input v-model="email" placeholder="email" /><br/>
+    <input v-model="password" placeholder="password" /><br />
+    <button @click="signUp">sign up</button><br />
+    <button id="sign-in-button">sign in</button><br />
+    <button @click="logout">logout</button>
+  </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue';
-import HelloWorld from './components/HelloWorld.vue';
+import { createUserWithEmailAndPassword, getAuth, RecaptchaVerifier, signInWithEmailAndPassword, signOut } from 'firebase/auth';
+import { initializeApp } from 'firebase/app';
 
 export default defineComponent({
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  data() {
+    return {
+      email: '',
+      password: '',
+      recaptchaVerifier: {} as any,
+    }
+  },
+
+  methods: {
+    signUp() {
+      console.log('sign up');
+      const auth = getAuth();
+      createUserWithEmailAndPassword(auth, this.email, this.password).then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      }).catch((err) => {
+        console.log(err);
+      })
+    },
+
+    signIn() {
+      console.log('sign in');
+      const auth = getAuth();
+      
+      signInWithEmailAndPassword(auth, this.email, this.password).then((userCredential) => {
+        const user = userCredential.user;
+        console.log(user);
+      }).catch((err) => {
+        console.log(err)
+      })
+    },
+
+    logout() {
+      console.log('logout');
+      const auth = getAuth();
+      signOut(auth).then(() => {
+        console.log('logouted');
+      }).catch((err) => {
+        console.log(err);
+      })
+    }
+  },
+
+  created() {
+    console.log('onCreated');
+    const firebaseConfig = {
+
+    };
+
+    const app = initializeApp(firebaseConfig);
+  },
+
+  mounted() {
+    console.log('on mouted');
+    const auth = getAuth();
+    this.recaptchaVerifier = window.recaptchaVerifier = new RecaptchaVerifier('sign-in-button', {
+      'size': 'invisible',
+      'callback': (response: any) => {
+        console.log('aaaaaaa');
+        // reCAPTCHA solved, allow signInWithPhoneNumber.
+        console.log(response);
+        this.signIn();
+      }
+    }, auth);
+    this.recaptchaVerifier.render();
+  },
 });
+
+declare global {
+  interface Window {
+    recaptchaVerifier?: any;
+  }
+}
+
 </script>
 
 <style>
